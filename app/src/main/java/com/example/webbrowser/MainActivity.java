77116f305566b090c36ae5ef68011627253dbae4
teys1s <alyoshkay21@gmail.com>
2019-03-02 00:02:18 +0200

    first commit

---
+++ app/src/main/java/com/example/webbrowser/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.webbrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        web = findViewById(R.id.webView);
+        WebSettings ws = web.getSettings();
+        ws.setJavaScriptEnabled(true);
+        web.loadUrl("https://www.google.com.ua");
+        web.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (web.canGoBack()) {
+            web.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
